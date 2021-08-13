95d60e4ae1df3c149179902e0ae35b3568562bd6
xorua117 <xorua117@gmail.com>
2021-08-13 15:38:43 +0900

    Updating onclickhandler

--- src/components/bookmarks/Bookmarks.js
+++ src/components/bookmarks/Bookmarks.js
@@ -20,7 +20,7 @@ function Bookmarks() {
     },
   ]);
 
-  function handler(e) {
+  function onClickHandler(e) {
     if (e.target.className === classes.item) {
       const child = e.target.parentNode.childNodes;
 
@@ -37,14 +37,22 @@ function Bookmarks() {
       }
 
       e.target.parentNode.classList.add(classes.clicked);
+    } else if (e.target.parentNode.parentNode.className === classes.item) {
+      const child = e.target.parentNode.parentNode.parentNode.childNodes;
+
+      for (let i = 0; i < child.length; i++) {
+        child[i].classList.remove(classes.clicked);
+      }
+
+      e.target.parentNode.parentNode.classList.add(classes.clicked);
     }
   }
 
   return (
     <div className={classes.bookmarks}>
       {myBookmarks.map((myBookmark) => (
-        <div className={classes.item} key={myBookmark.id} onClick={handler}>
-          <img src={myBookmark.icon} />
+        <div className={classes.item} key={myBookmark.id} onClick={onClickHandler}>
+          <img src={myBookmark.icon} alt={icon}/>
           <div className={classes.text}>
             <div>{myBookmark.title}</div>
             <div className={classes.url}>{myBookmark.url}</div>
